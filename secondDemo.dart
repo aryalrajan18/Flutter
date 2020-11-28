69cd7c2f37733bd3583f950d4b79ebad8d548307
aryalrajan18 <71181375+aryalrajan18@users.noreply.github.com>
2020-11-28 12:51:07 +0545

    Create secondDemo.dart

---
+++ secondDemo.dart
@@ -0,0 +1,86 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: NinjaCard(),
+    ));
+
+class NinjaCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text('hello this is first project'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/raj.jpg'),
+                radius: 40.0,
+              ),
+            ),
+            Divider(
+              height: 20.0,
+              color: Colors.grey[600],
+            ),
+            Text('Name',
+                style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0,
+                  fontSize: 14.0,
+                )),
+            SizedBox(height: 10.0),
+            Text('rajan',
+                style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold,
+                )),
+            SizedBox(height: 10.0),
+            Text('Address',
+                style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0,
+                  fontSize: 14.0,
+                )),
+            SizedBox(height: 10.0),
+            Text('Pokhara 11 fulbari',
+                style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold,
+                )),
+            SizedBox(height: 10.0),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                Text(
+                  'helo@gmail.com',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 20.0,
+                    letterSpacing: 2.0,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
